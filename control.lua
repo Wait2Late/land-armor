91bf57a11bedac68ae4b31c3bbcceda037778490
Wait2Late <w82l8.islam@gmail.com>
2024-07-12 18:30:57 +0200

    Testing with more entities
    Seems like concretes are not entities likewise with iron-plate

--- control.lua
+++ control.lua
@@ -13,7 +13,13 @@ script.on_event(defines.events.on_player_changed_position,
       -- player.surface.create_entity{name="concrete", position=player.position, force="neutral", recipe = "concrete"} 
       -- game.surfaces[1].create_entity{name = "inserter", position = game.player.position, direction = defines.direction.north}
       
-       player.surface.create_entity{name="inserter", position=player.position, direction = defines.direction.north, force="neutral"} -- OG
+      -- Creates inserters. Also learned force is a matter of which faction is which.
+      --  player.surface.create_entity{name="inserter", position=player.position, direction = defines.direction.north, force="player"} 
+       player.surface.create_entity{name="steel-chest", position={player.position.x, player.position.y-3},
+        direction = defines.direction.north, force="player"} 
+
+
+      --  player.surface.create_entity{name="concrete", position=player.position, direction = defines.direction.north}
 
 
     end
