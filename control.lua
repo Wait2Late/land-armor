c375415867f319affe11e04bb90739037d89c998
Wait2Late <w82l8.islam@gmail.com>
2024-07-12 16:18:42 +0200

    It now creates inserters

--- control.lua
+++ control.lua
@@ -7,9 +7,15 @@ script.on_event(defines.events.on_player_changed_position,
     if player.character and player.get_inventory(defines.inventory.character_armor).get_item_count("land-armor") >= 1 then
        -- create the fire where they're standing
 
-       player.surface.create_entity{name="land-fill", position=player.position, force="neutral"}
+      --  player.surface.create_entity{name="land-fill", position=player.position, force="neutral"}
        
-      --  player.surface.create_entity{name="fire-flame", position=player.position, force="neutral"} 
+      --  player.surface.create_entity{name="fire-flame", position=player.position, force="neutral"} -- OG
+      -- player.surface.create_entity{name="concrete", position=player.position, force="neutral", recipe = "concrete"} 
+      -- game.surfaces[1].create_entity{name = "inserter", position = game.player.position, direction = defines.direction.north}
+      
+       player.surface.create_entity{name="inserter", position=player.position, direction = defines.direction.north, force="neutral"} -- OG
+
+
     end
   end
 )
